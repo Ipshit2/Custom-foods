5a3d0b4bec7251fa021388c393b8d8940374f077
Ipshit <ipshithaste@gmail.com>
2025-04-17 13:51:39 +0530

    Initial Commit

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -6,8 +6,8 @@ interface NAME{
 
 function Navbar({ name } :NAME) {
   return (
-    <div className='pt-[20px] pl-[20px] text-[20px] flex  text-[#E9E1D4] gap-x-[900px]'>
-        <h1 className=' rounded-2xl w-[280px] px-[25px] py-[7px] border-[#201E1F] border-[4px] bg-[#423C3C]' >CustomFoods</h1>
+    <div className='pt-[20px] pl-[20px] text-[15px] flex  text-[#E9E1D4] gap-x-[900px]'>
+        <h1 className=' rounded-2xl w-[170px] py-[7px] border-[#201E1F] border-[4px] bg-[#423C3C]' >CustomFoods</h1>
         <NavLink to={name === "Order" ? "/orders" : "/dashboard"}>
         <h1 className=' rounded-2xl w-auto px-[25px] py-[7px] border-[#201E1F] border-[4px] bg-[#423C3C]' >
         {name === "Order" ? "Orders" : "Dashboard"}
--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -69,7 +69,7 @@ function Dashboard() {
         <div className='bg-[#EEDEC4] text-[#66422A] h-screen w-full font-P2P '>
             <Navbar name="Order" />
             <div className='flex justify-center items-center bg-[#423C3C] border-[black] border-2 text-[#E9E1D4] rounded-t-3xl rounded-b-sm mx-[140px] mt-[30px] mb-[5px]'>
-                <h1 className='my-[18px] text-[15px] '>CUSTOMIZE YOUR OWN PIZZA..!!</h1>
+                <h1 className='my-[15px] text-[20px] '>&gt;&gt; CUSTOMIZE YOUR OWN PIZZA... &lt;&lt;</h1>
             </div>
             <div className='mx-[140px] bg-[#E5CA95] border-[#66422A] border-4 p-[30px] rounded-sm'>
                 <div className='flex justify-center items-center w-full'>
