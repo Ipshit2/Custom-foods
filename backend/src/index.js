2508887064ed1b69ee1bf2d89f6ff670a9b53e96
Ipshit <ipshithaste@gmail.com>
2026-03-31 22:18:57 +0530

    port fiixed

--- backend/src/index.js
+++ backend/src/index.js
@@ -8,7 +8,7 @@ import connectDB from "./db/db.config.js"
 
 connectDB()
 .then(() => {
-    app.listen(process.env.FRONTEND_URL || "http://localhost:5173", () => {
+    app.listen(process.env.PORT, () => {
         console.log(`⚙️ Server is running at port : ${process.env.PORT}`);
         console.log(process.env.PORT);
         
